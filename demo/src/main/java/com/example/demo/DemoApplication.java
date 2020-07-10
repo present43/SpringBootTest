5b0a824b633d1927626c0450f017870038bfaec6
qianxiangwen <qianxiangwen@sto.cn>
2020-07-10 14:28:56 +0800

    dev-01 添加包扫描

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages="com.exampl.demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
